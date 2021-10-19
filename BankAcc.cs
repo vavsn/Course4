73f0b6bcb8b81fad85edd5f87040f5804acfb6ed
vavsn <41681585+vavsn@users.noreply.github.com>
2021-10-19 21:44:11 +0700

    HomeWork. Task 1. working with a bank account. Using properties.

---
+++ BankAcc.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// класс реализации 1 упражнения домашнего задания
+    /// https://gb.ru/lessons/180797/homework
+    /// </summary>
+    public class BankAcc
+
+    {
+        public enum TypeBankAccount : int
+        {
+            //дебетовый
+            Deb = 1,
+            //кредитный
+            Cred,
+            //текущий
+            Curr
+        }
+
+        // количество символов в номере счета
+        private static int _numsymbols = 25;
+
+        // номер счета
+        private string _numaccount;
+        // баланс
+        private double _balance;
+        // тип банковского счета:
+        // - дебетовый
+        // - кредитный
+        // - текущий
+        private TypeBankAccount _typebankaccount;
+
+        public void SetNumAccount(string numacc)
+        {
+            if (numacc.Length > _numsymbols)
+            { 
+                _numaccount = numacc.Substring(0, _numsymbols);
+                return;
+            }
+            _numaccount = numacc.PadLeft(_numsymbols, '0');
+        }
+
+        public string GetNumAccount()
+        {
+            return _numaccount;
+        }
+
+        public void SetBalance(double balance)
+        {
+            try
+            {
+                _balance = balance;
+            }
+            catch
+            { 
+            }
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public void SetTypeBankAccount(TypeBankAccount typebankaccount)
+        {
+            try
+            {
+                _typebankaccount = typebankaccount;
+            }
+            catch
+            { 
+            }
+        }
+
+        public TypeBankAccount GetTypeBankAccount()
+        {
+            return _typebankaccount;
+
+        }
+
+        public string ToString()
+        {
+            return "Номер счета: " + this._numaccount + "\nБаланс: " + this._balance + 
+                "\nТип счета: " + this._typebankaccount.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAcc ba = new BankAcc();
+            ba.SetNumAccount("123456789_123456789_123456789_192773_09");
+            ba.SetBalance((double)123.34);
+            ba.SetTypeBankAccount(BankAcc.TypeBankAccount.Cred);
+            Console.WriteLine("Данные счета:");
+            Console.WriteLine(ba.ToString());
+        }
+    }
+}
