b77a58e80331429d7572009fc29293ac15919514
vavsn <41681585+vavsn@users.noreply.github.com>
2021-10-20 22:55:13 +0700

    Homework. Task 1. The method transfer from account to account.

---
+++ BankAcc.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// класс реализации 1 упражнения домашнего задания
+    /// https://gb.ru/lessons/180797/homework
+    /// </summary>
+    public class BankAcc
+
+    {
+        public enum TypeBankAccount : int
+        {
+            //дебетовый
+            Deb = 1,
+            //кредитный
+            Cred,
+            //текущий
+            Curr
+        }
+
+        // количество символов в номере счета
+        private static int _numsymbols = 25;
+
+        // номер счета
+        private static string _numaccount;
+
+        // баланс
+        private double _balance;
+        // тип банковского счета:
+        // - дебетовый
+        // - кредитный
+        // - текущий
+        private TypeBankAccount _typebankaccount;
+
+        public string numaccount
+        {
+            set 
+            {
+                _numaccount = string.Empty;
+                int[] loc = new int[25];
+                Random rnd = new Random();
+
+                for (int i = 0; i < _numsymbols; i++)
+                {
+                    loc[i] = rnd.Next(0, 9);
+                }
+                _numaccount = String.Concat<int>(loc);
+            }
+            get 
+            {
+                return _numaccount;
+            }
+        }
+
+        public double balance
+        {
+            set 
+            {
+                try
+                {
+                    _balance = Math.Round(value, 2);
+                }
+                catch
+                {
+                }
+            }
+            get 
+            {
+                return _balance;
+            }
+        }
+        public TypeBankAccount typebankaccount
+        {
+            set
+            {
+                try
+                {
+                    _typebankaccount = value;
+                }
+                catch
+                {
+                }
+            }
+            get
+            {
+                return _typebankaccount;
+            }
+        }
+
+        public BankAcc() 
+        {
+            numaccount = "";
+            balance = 0.0;
+            typebankaccount = TypeBankAccount.Curr;
+        }
+        public BankAcc(double bal)
+        {
+            numaccount = "";
+            balance = bal;
+            typebankaccount = TypeBankAccount.Curr;
+        }
+        public BankAcc(TypeBankAccount tba)
+        {
+            numaccount = "";
+            balance = 0.0;
+            typebankaccount = tba;
+        }
+        public BankAcc(double bal, TypeBankAccount tba)
+        {
+            numaccount = "";
+            balance = bal;
+            typebankaccount = tba;
+        }
+        public string ToString()
+        {
+            return "Номер счета: " + this.numaccount + "\nБаланс: " + this.balance + 
+                "\nТип счета: " + this.typebankaccount.ToString();
+        }
+
+        public void Transfer(
+            BankAcc OutBankAcc, // банковский счет, источник перевода
+            double Summ         // сумма перевода
+            )
+        {
+            if (OutBankAcc is null)
+            {
+                throw new Exception("Не указан номер счета");
+            }
+            if (Summ <= 0)
+            {
+                throw new Exception("Не указана либо отрицательная сумма перевода");
+            }
+            if (OutBankAcc.balance < Summ)
+            {
+                throw new Exception("На счету недостаточно средств");
+            }
+
+            balance += Summ;
+            OutBankAcc.balance -= Summ;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAcc ba = new BankAcc(100, BankAcc.TypeBankAccount.Deb);
+            Console.WriteLine("Данные счета1:");
+            Console.WriteLine(ba.ToString());
+            BankAcc ba1 = new BankAcc(548.213, BankAcc.TypeBankAccount.Deb);
+            Console.WriteLine("Данные счета2:");
+            Console.WriteLine(ba1.ToString());
+            try
+            {
+                ba.Transfer(ba1, 600);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Отказ: {e.Message}");
+                Console.WriteLine();
+            }
+            Console.WriteLine("Данные счета1 после операции:");
+            Console.WriteLine(ba.ToString());
+        }
+    }
+}
