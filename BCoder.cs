079ab7ea11c40d7c67ab0f9033cfd3cde0d8b871
vavsn <41681585+vavsn@users.noreply.github.com>
2021-11-24 22:57:08 +0700

    Homework. Usage interfaces, inheritance and structures.

---
+++ BCoder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coder
+{
+    class BCoder : ICoder
+    {
+        StructChar scA = new StructChar('a');
+        StructChar scR = new StructChar('r');
+        List<char> lcA;
+        List<char> lcA_revers;
+
+        private int _ind;
+
+        private int Ind 
+        {
+            get { return _ind; }
+            set 
+            {
+                _ind = value;
+            }
+        }
+
+        private StructChar SearchStruct(char _ch)
+        {
+            int ind = scR.C().IndexOf(_ch);
+            if (ind != -1)
+            {
+                return scR;
+            }
+            return scA;
+        }
+        private char FindChar(char _ch, bool sign)
+        {
+            char c;
+            int j = 0;
+            StructChar s = SearchStruct(_ch);
+            lcA = s.C();
+            lcA_revers = s.IC();
+            Ind = lcA.IndexOf(_ch);
+            if (Ind == -1)
+            {
+                return _ch;
+            }
+            c = sign ? lcA_revers[lcA.IndexOf(_ch)] : lcA[lcA_revers.IndexOf(_ch)];
+            return c;
+        }
+
+        private string MultiCoder(string s, bool sign)
+        {
+            char[] ch = s.ToCharArray();
+
+            for (int i = 0; i < ch.Length; i++)
+            {
+                ch[i] = FindChar(ch[i], sign);
+            }
+
+            return new string(ch);
+        }
+        public string Decode(string s)
+        {
+            return MultiCoder(s, false);
+            throw new NotImplementedException();
+        }
+
+        public string Encode(string s)
+        {
+            return MultiCoder(s, true);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StructChar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coder
+{
+    struct StructChar
+    {
+        private List<char> _c;
+        private List<char> _ic;
+
+        public StructChar(char a)
+        {
+            _c = new List<char>();
+            if (a == 'a')
+            {
+                for (int i = 65; i < 91; i++)
+                {
+                    _c.Add((char)i);
+                }
+                for (int i = 97; i < 123; i++)
+                {
+                    _c.Add((char)i);
+                }
+            }
+            if (a == 'r')
+            {
+                for (int i = 1040; i < 1104; i++)
+                {
+                    _c.Add((char)i);
+                }
+            }
+            _ic = new List<char>(_c);
+            int _count = _c.Count / 2;
+            _ic.Reverse(0, _count);
+            _ic.Reverse(_count, _count);
+        }
+
+        public List<char> C()
+        {
+            return _c;
+        }
+
+        public List<char> IC()
+        {
+            return _ic;
+        }
+
+    }
+}
